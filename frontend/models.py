5fe7077393dcb0ebcf0f9aa3a7fa6321a0b2ce1b
e-landeros <landerosedgard@gmail.com>
2019-04-11 01:15:43 -0700

    front end template + views

--- frontend/models.py
+++ frontend/models.py
@@ -1,3 +1,31 @@
+from __future__ import unicode_literals
+
+from django.urls import reverse
 from django.db import models
 
-# Create your models here.
+
+class Car(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='car_images')
+    description = models.TextField()
+    daily_rent = models.IntegerField()
+
+    is_available = models.BooleanField()
+
+    def get_absolute_url(self):
+        return reverse('car-details', kwargs={'pk': self.pk})
+
+
+class Booking(models.Model):
+    car = models.ForeignKey(Car)
+
+    customer_name = models.CharField(max_length=100)
+    customer_email = models.EmailField()
+    customer_phone_number = models.TextField()
+
+    booking_start_date = models.DateField()
+    booking_end_date = models.DateField()
+    booking_message = models.TextField()
+
+    is_approved = models.BooleanField()
+
